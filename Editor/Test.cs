27b9b4f25035fcbcc720c64cc30e5f270f5e9085
katsumasa <katsumasa@unity3d.com>
2021-03-26 16:05:42 +0900

    大幅改修

---
+++ Editor/Test.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Profiling.Memory.Experimental;
+
+
+public  class Test
+{
+    static Action<string, bool> action;
+
+    [MenuItem("Window/TEST")]
+    public static void Func()
+    {
+
+        //        System.Type t = Types.GetType("Unity.MemoryProfiler.Editor", "Unity.MemoryProfiler.Editor.dll");
+        //var assembly =  Assembly.Load("Unity.MemoryProfiler.Editor");
+        //var type = assembly.GetType("Unity.MemoryProfiler.Editor.MemoryProfilerWindow");
+#if false
+        var memoryProfilerWindow = type.InvokeMember(
+                   null
+               , BindingFlags.CreateInstance
+               , null
+               , null
+               , new object[] { }
+               );
+
+        
+        var methodInfo = type.GetMethod("TakeCapture", BindingFlags.NonPublic | BindingFlags.Instance);
+        methodInfo.Invoke(memoryProfilerWindow, null);
+#endif
+        //var window = EditorWindow.GetWindow(type);
+        //var methodInfo = type.GetMethod("TakeCapture", BindingFlags.NonPublic | BindingFlags.Instance);
+        //var methodInfo = type.GetMethod("DelayedSnapshotRoutine", BindingFlags.NonPublic | BindingFlags.Instance);
+
+
+
+        //        methodInfo.Invoke(window, null);
+        //      Debug.Log(window);
+        var now = System.DateTime.Now;        
+        string fpath = System.IO.Directory.GetCurrentDirectory() + "/Temp/" + now.ToString("yyyyMMddHHmmss") + ".snap";        
+        MemoryProfiler.TakeSnapshot(fpath, FinishCB, ScreenshotCallback);
+
+    }
+
+
+    static void FinishCB(string fpath,bool isSuccess)
+    {
+        Debug.Log(fpath);
+        Debug.Log(isSuccess);
+    }
+
+    static void ScreenshotCallback(string fpath, bool isSuccess,UnityEngine.Profiling.Experimental.DebugScreenCapture debugScreenCapture)
+    {
+        Debug.Log(fpath);
+        Debug.Log(isSuccess);
+        Texture2D texture = new Texture2D(debugScreenCapture.width, debugScreenCapture.height, debugScreenCapture.imageFormat, false);
+        try
+        {
+            var bytes = debugScreenCapture.rawImageDataReference.ToArray();
+            texture.LoadRawTextureData(bytes);
+            var pngData = texture.EncodeToPNG();
+            var path = System.IO.Path.GetFileNameWithoutExtension(fpath) + ".png";
+            System.IO.File.WriteAllBytes(path, pngData);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+
+
+    static void CallBack(string s,bool flag)
+    {
+        Debug.Log(s);
+        Debug.Log(flag);
+        
+    }
+}
---
+++ Samples/Scripts/SceneSub.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utj.UnityMemoryProfilerSupportKun;
+
+//
+//　シーン切り替え時にMemoryProfilerのSnapShotを実行するSampleのサブ
+//
+public class SceneSub : MonoBehaviour {
+
+    delegate void Task();
+
+    [SerializeField] GameObject m_cubePrefabs;
+    [SerializeField] string m_nextSceneName;
+    float m_time;
+    Task m_task;
+    GameObject[] m_cubeObjects;    
+    float m_lastInterval;
+    static int m_snapShotNo = 0;
+
+
+    private void Awake()
+    {
+        m_lastInterval = 0;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        m_cubeObjects = new GameObject[10 * 10 * 10];
+        var i = 0;
+        for (var z = 0; z < 10; z++)
+        {
+            for (var y = 0; y < 10; y++)
+            {
+                for (var x = 0; x < 10; x++)
+                {
+                    var gameObject = Instantiate(m_cubePrefabs) as GameObject;
+                    Vector3 v3 = new Vector3(x * 5.0f, y * 5.0f, z * 5.0f);
+                    gameObject.transform.localPosition = v3;
+                    m_cubeObjects[i++] = gameObject;
+                }
+            }
+        }
+        m_time = Time.realtimeSinceStartup;
+        m_task = Task0001;
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+		if(m_task != null)
+        {
+            m_task();
+        }
+	}
+
+
+    void Task0001()
+    {
+        var currentTime = Time.realtimeSinceStartup;
+        var delta = currentTime - m_time;
+        if (delta > 3.0f)
+        {
+            UnityMemoryProfilerSupportKunClient.instance.Send(string.Format("{0:00000A}", m_snapShotNo++));
+            m_task = Task0002;
+        }
+    }
+
+    void Task0002 ()
+    {
+        var timeNow = Time.realtimeSinceStartup;
+        var waitTime = timeNow - m_lastInterval;
+        if ((UnityMemoryProfilerSupportKunClient.instance.isDone == true) || (waitTime > 10.0f))
+        {
+            for(var i = 0; i < m_cubeObjects.Length; i++)
+            {
+                Destroy(m_cubeObjects[i]);
+                m_cubeObjects[i] = null;
+            }
+            m_cubeObjects = null;
+            SceneMain.instance.ChangeScene(m_nextSceneName);
+            m_task = null;
+        }
+    }
+}
